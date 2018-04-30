1c15d808c60086aaec8027b20c405dd271b65b18
Hassam7 <hassam.ali7@hotmail.com>
2018-04-30 16:31:51 +0500

    Complete Redis

--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,41 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const redisUrl = 'redis://127.0.0.1:6379';
+
+const client = redis.createClient(redisUrl);
+const util = require('util');
+client.hget = util.promisify(client.hget)
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-  console.log('Running exec');
-  return exec.apply(this, arguments);
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+  return this;
+}
+
+mongoose.Query.prototype.exec = async function() {
+  console.log('exec', this.useCache)  
+  if (!this.useCache) return exec.apply(this, arguments);
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  }));
+
+  const cachedValue = await client.hget(this.hashKey, key);
+
+  if (cachedValue) {
+    const doc = JSON.parse(cachedValue);
+    return Array.isArray(doc) ? doc.map( m => new this.model(m)) : new this.model(doc);
+  }
+
+
+  const result = await exec.apply(this, arguments);
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
+  return result;
 } 
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
+}
