b9b171d9c09244874b2b3601929e5beab03223c8
Hassam7 <hassam.ali7@hotmail.com>
2018-05-01 16:13:49 +0500

    Ci Setup Complete

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: '758577298301-vv2evodtt65lof68rbljnp50p9nmn1ko.apps.googleusercontent.com',
+	googleClientSecret: 'dZCev_riwXMqLO625nrpJswp',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl = 'redis://127.0.0.1:6379',
+  
+};
